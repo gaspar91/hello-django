82de188d727d91cc804f3cf66d72a0c98de3d39d
gaspar91 <andreiafg.91@gmail.com>
2020-11-19 14:04:23 +0000

    added table

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    # this last items is the items variable we've just created
+    return render(request, 'todo/todo_list.html', context)
